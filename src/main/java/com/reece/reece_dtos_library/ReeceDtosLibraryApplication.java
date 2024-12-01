2f2bdb99d64226b6683f8b82f944d561c2b58ef1
angelrha12 <angel.hernandez@morsco.com>
2024-11-30 21:43:31 -0600

    Add all changes for GitHub Packages

---
+++ src/main/java/com/reece/reece_dtos_library/ReeceDtosLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.reece.reece_dtos_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeceDtosLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReeceDtosLibraryApplication.class, args);
+	}
+
+}
